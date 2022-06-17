beae2de75d2506f5f5eeb40dee6fa8c9985aea56
Lev <107544957+LevSvl8@users.noreply.github.com>
2022-06-17 15:54:09 +0300

    Add files via upload

---
+++ copy_dir1.0.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+def list_of_files(dir_from1,dir_to1):
+    global dir_from,dir_to
+    dir_from, dir_to = dir_from1,dir_to1
+    list_of_f = os.listdir(dir_from1)
+    scan_list(list_of_f)
+    return list_of_f
+
+def scan_list(list):
+    for file in list:
+        if os.path.isdir(dir_from+file) != True:
+            shutil.copy(dir_from+file,dir_to)
+    else:
+        os.mkdir(dir_to+file)
+        list_of_files(dir_from+file,dir_to+file)
+
+
+
+
+dir1 = 'C:\\proects/'
+dir2 = 'C:\\PerfLogs/'
+
+list_of_files(dir1,dir2)
