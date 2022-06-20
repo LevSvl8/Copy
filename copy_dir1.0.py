17418405b89c4bf834166e1de52deabd894e7911
LevSvl8 <svetlakov_l@bk.ru>
2022-06-20 10:43:56 +0300

    copydir1.1

--- copy_dir1.0.py
+++ copy_dir1.0.py
@@ -11,15 +11,15 @@ def list_of_files(dir_from1,dir_to1):
 def scan_list(list):
     for file in list:
         if os.path.isdir(dir_from+file) != True:
-            shutil.copy(dir_from+file,dir_to)
-    else:
-        os.mkdir(dir_to+file)
-        list_of_files(dir_from+file,dir_to+file)
+            shutil.copy(dir_from+'/'+file,dir_to)
+        else:
+            os.mkdir(dir_to+file)
+            list_of_files(dir_from+file,dir_to+file)
+            print(dir_to+file)
 
 
 
-
-dir1 = 'C:\\proects/'
-dir2 = 'C:\\PerfLogs/'
+dir1 = 'C:\\Users\\Work\\Documents\\git-learn/'
+dir2 = 'C:\\Users\\Work\\Documents\\test2/'
 
 list_of_files(dir1,dir2)
